cc1dd2c40acd5bdf3ba63d716b96776a3aa9a018
usuario <usuario@DESKTOP-EIGGSHJ>
2023-04-22 17:04:09 -0300

    Proyecto tp2 inicial

---
+++ TP2_Grupo26/src/main/java/ar/edu/unju/fi/Tp2Grupo26Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo26Application.class, args);
+	}
+
+}
